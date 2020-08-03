237588316e37f8eabf68accd9205c901b2aa9d22
Annie <anqi.chen@thoughtworks.com>
2020-08-03 16:11:25 +0800

    [anqi] commit mommifierTest 2.0

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -8,13 +8,14 @@ public class Mommifier {
     public String insertMommy(String str) {
         int length = str.length();
         String result = "";
+        String vowel = "[aeiou]";
         List<String> list = Arrays.asList(str.split(""));
         List<String> newList = new ArrayList<>(list);
-        long vowelLength = newList.stream().filter(s -> s.matches("[aeiou]")).count();
+        long vowelLength = newList.stream().filter(s -> s.matches(vowel)).count();
         if (vowelLength > length*0.3) {
             for (int i =0; i < newList.size()-1; i++) {
-                if (newList.get(i).matches("[aeiou]") && newList.get(i+1).matches("[aeiou]")) {
-                    newList.add(i+1," mommy ");
+                if (newList.get(i).matches(vowel) && newList.get(i+1).matches(vowel)) {
+                    newList.add(i+1,"mommy");
                 }
             }
         }
@@ -24,9 +25,4 @@ public class Mommifier {
         return result;
     }
 
-//    public static void main(String[] args) {
-//        String str = "aeiou";
-//        Mommifier mom = new Mommifier();
-//        System.out.println(mom.insertMommy(str));
-//    }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -3,12 +3,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MommifierTest {
+    Mommifier mommifier = new Mommifier();
     @Test
-    void should_insert_mommy_when_given_string_has_more_vowels() {
+    void should_return_string_when_input_has_not_enough_vowels() {
+        String str = "string";
+        final String result = mommifier.insertMommy(str);
+        assertEquals("string",result);
+    }
+
+    @Test
+    void should_return_string_when_input_has_many_vowels_but_not_continuous_vowels() {
+        String str = "hello";
+        final String result = mommifier.insertMommy(str);
+        assertEquals("hello",result);
+    }
+
+    @Test
+    void should_insert_mommy_when_given_string_has_many_and_continuous_vowels() {
         //given
         String str = "aeiou";
-        String expected = "a mommy e mommy i mommy o mommy u";
-        Mommifier mommifier = new Mommifier();
+        String expected = "amommyemommyimommyomommyu";
         // when
         final String result = mommifier.insertMommy(str);
         //then
